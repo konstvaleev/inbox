cda42817ab06f31288ada9ee56b095fa2de013ca
Valeev <konstantin.valeev@wirecard.com>
2022-06-24 23:25:23 +0800

    deploy contract to rinkeby

--- deploy.js
+++ deploy.js
@@ -1,2 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
 
-// deploy code will go here
+const provider = new HDWalletProvider(
+    'drum rural shift imitate title talent switch cupboard print kid update helmet',
+    'https://rinkeby.infura.io/v3/f2d9f126adef4e808467bd68d8774d3e'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    // a helper function to be able to use async mode
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there'] })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);
+
+    provider.engine.stop();
+
+}
+deploy();
