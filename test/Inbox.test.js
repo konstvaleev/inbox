d2c0f5d79cfa03d582ab91630a7f7006206e286f
kv <konstantin.valeev@wirecard.com>
2022-06-24 23:56:14 +0800

    test changes

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -10,12 +10,13 @@ let inbox;
 beforeEach(async ()   => {
     // Get a list of all accounts
     accounts = await web3.eth.getAccounts();
-    
+
     // Use one of those accounts to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode, arguments: ['Hi there!'] })
         .send({ from: accounts[0], gas: '1000000' });
 });
+
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
