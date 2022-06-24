27c70cae3e27c8be371ee67b33dcf2c5425fe162
Valeev <konstantin.valeev@wirecard.com>
2022-06-24 23:53:14 +0800

    updates tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -10,12 +10,11 @@ let inbox;
 beforeEach(async ()   => {
     // Get a list of all accounts
     accounts = await web3.eth.getAccounts();
-
+    
+    // Use one of those accounts to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode, arguments: ['Hi there!'] })
         .send({ from: accounts[0], gas: '1000000' });
-    // Use one of those accounts to deploy
-    // the contract
 });
 
 describe('Inbox', () => {
